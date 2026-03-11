77429e4ff22dc85d971315ef2f0223b1341354eb
TimofeyMeshcheryakov <tdmesherykov@mail.ru>
2026-03-11 09:19:12 +0300

    Step 7 arrays

---
+++ code/Step7_massif_max_element/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Поиск максимума");
+int n = 10;
+int[] X = new int[n];
+int Max = X[0]; 
+int NMax = 1;
+
+for (int i = 1; i < n; i++)
+{
+    if (X[i] > Max)
+    {
+        Max = X[i];
+        NMax = i + 1;
+    }
+}
+
+Console.WriteLine($"Максимальное значение (Max): {Max}");
+Console.WriteLine($"Номер максимального элемента (NMax): {NMax}");
